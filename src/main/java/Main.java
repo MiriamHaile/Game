1a6303f70f285e6bf0c7094ee3c3443b67ca8fb8
Miriam Haile <miriam.haile@hotmail.com>
2019-01-23 14:31:03 +0100

    change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
+
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+        Terminal terminal = terminalFactory.createTerminal();
+        terminal.setCursorVisible(false);
+    }
 }
