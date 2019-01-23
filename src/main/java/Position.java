e78e65ae349c1b97295419bd9a0871e74d8a1147
Miriam Haile <miriam.haile@hotmail.com>
2019-01-23 15:41:26 +0100

    new constructor

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,2 +1,12 @@
+
 public class Position {
+    protected int column;
+    protected int row;
+
+
+    public Position(int column, int row) {
+        this.column = column;
+        this.row = row;
+    }
+
 }
